e8468bd71d7b9d5caf6317f082716e04bc86a875
takebul <takebulislam@gmail.com>
2026-04-07 19:55:09 +0600

    added product management

--- src/components/ProductManagement/ProductTable.jsx
+++ src/components/ProductManagement/ProductTable.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 
-const ProductTable = () => {
+const ProductTable = ({ products }) => {
   return (
     <div>
-      <h1></h1>
+      <h3>Products: {products.length} </h3>
+
+      <table>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, index) => (
+            <tr key={index}>
+              <td>{index + 1} </td>
+              <td>{product.name} </td>
+              <td>{product.price} </td>
+              <td>{product.quantity} </td>
+              <td></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
